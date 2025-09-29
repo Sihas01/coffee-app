4db98c5aa2aea2671612b9f62914c9177b650bf9
Sihas01 <sihasmandipa77@gmail.com>
2025-09-29 23:15:18 +0530

    Created Custom Top Bar

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,3 +1,4 @@
+import 'package:coffee_app/widget/CustomTopBar.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -9,52 +10,7 @@ class HomeScreen extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Custom AppBar',
       theme: ThemeData(useMaterial3: true),
-      home: Scaffold(
-        appBar: AppBar(
-          toolbarHeight: 225,
-          backgroundColor: const Color(0xff794028),
-          flexibleSpace: Stack(
-            children: [
-              Align(
-                alignment: Alignment.bottomRight,
-                child: Image.asset('asset/images/coffee.png'),
-              ),
-
-              Padding(
-                padding: const EdgeInsets.only(top: 40, right: 25,left: 25),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Hello, John",
-                          style: Theme.of(context).textTheme.headlineMedium
-                              ?.copyWith(color: Colors.white),
-                        ),
-                        Text(
-                          "Good Morning",
-                          style: Theme.of(
-                            context,
-                          ).textTheme.bodyLarge?.copyWith(color: Colors.white),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Icon(Icons.settings),
-                        Icon(Icons.notifications_none),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      home: Scaffold(appBar: CustomTopBar()),
     );
   }
 }
---
+++ lib/widget/CustomTopBar.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+class CustomTopBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomTopBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 225,
+        elevation: 4,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(20),
+            bottomRight: Radius.circular(20),
+          ),
+        ),
+        backgroundColor: const Color(0xff794028),
+        flexibleSpace: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Image.asset('asset/images/coffee.png'),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 40, right: 25, left: 25),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 12),
+                            child: Text(
+                              "Hello, John",
+                              style: Theme.of(context).textTheme.headlineMedium
+                                  ?.copyWith(color: Colors.white),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8.0),
+                            child: Text(
+                              "Good Morning",
+                              style: Theme.of(context).textTheme.bodyLarge
+                                  ?.copyWith(color: Colors.white),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Icon(
+                              Icons.dark_mode_outlined,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Icon(
+                              Icons.notifications_none,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Material(
+                            elevation: 2,
+                            borderRadius: BorderRadiusDirectional.circular(30),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: 'Search..',
+                                prefixIcon: Icon(Icons.search),
+                                border: InputBorder.none,
+                                contentPadding: EdgeInsets.symmetric(
+                                  horizontal: 16,
+                                  vertical: 14,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(225);
+}
